d70701c83c8f14bf96b361760c7611b115a5a83b
abdusamadweb <abdusamadweb@gmail.com>
2022-12-06 14:58:02 +0500

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.scss'
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="container">
+                <div className="header__inner">
+                    header
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
